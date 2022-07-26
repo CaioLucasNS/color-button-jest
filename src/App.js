1b3257e89da62859548547432cd46edd2052f687
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-07-26 14:42:27 -0300

    added test for checkbox disabling button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isChecked, setIsChecked] = useState(false);
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={isChecked}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input
+        type="checkbox"
+        onChange={() => setIsChecked(!isChecked)}
+        checked={isChecked}
+      />
     </div>
   );
 }
